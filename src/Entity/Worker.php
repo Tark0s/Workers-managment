5f201fc633a3780bf828d1a4cb512e1061ecf280
Lukasz <tarkowski14@gmail.com>
2024-10-04 16:57:41 +0200

    Add worker entity

---
+++ src/Entity/Worker.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WorkerRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\Doctrine\UuidGenerator;
+use Ramsey\Uuid\UuidInterface;
+
+#[ORM\Entity(repositoryClass: WorkerRepository::class)]
+class Worker
+{
+    #[ORM\Id]
+    #[ORM\Column(type: "uuid", unique: true)]
+    #[ORM\GeneratedValue(strategy: "CUSTOM")]
+    #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
+    protected UuidInterface $id;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    public function getId(): UuidInterface
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
